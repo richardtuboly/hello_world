07de85405b850e55a1259ca40271d82ef9741ac5
richardtuboly <richard.tuboly@icloud.com>
2025-12-04 00:57:04 +0100

    feat: add /health endpoint

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World! Have a great day!"
 
+@app.route("/health")
+def health():
+    return "OK"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
